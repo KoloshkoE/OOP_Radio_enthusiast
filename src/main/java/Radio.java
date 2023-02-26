fcd18cfa8c1f54f67cc6f9766d55beaf74939505
Koloshko Evgeni <kolohkoe@gmail.com>
2023-02-26 13:20:29 +0700

    Homework is corrected

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,28 +1,59 @@
 public class Radio {
-    public int currentVolume;
-    public int currentRadioStation;
+    private int currentVolume;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newcurrentVolume) {
+        if (newcurrentVolume < 0) {
+            return;
+        }
+        if (newcurrentVolume > 10) {
+            return;
+        }
+        this.currentVolume = newcurrentVolume;
+    }
+
+    private int currentRadioStation;
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int newcurrentRadioStation) {
+        this.currentRadioStation = newcurrentRadioStation;
+    }
 
     public void prev() {
+        int target;
         if (currentRadioStation > 0) {
-            currentRadioStation = currentRadioStation - 1;
-        } else {currentRadioStation = 9;}
+            target = currentRadioStation - 1;
+            setCurrentRadioStation(target);
+        } else {
+            setCurrentRadioStation(9);
+        }
     }
+
     public void next() {
+        int target;
         if (currentRadioStation < 9) {
-            currentRadioStation = currentRadioStation + 1;
-        } else {currentRadioStation = 0;}
+            target = currentRadioStation + 1;
+            setCurrentRadioStation(target);
+        } else {
+            setCurrentRadioStation(0);
+        }
     }
 
     public void lowerVolume() {
-        if (currentVolume > 0) {
-            currentVolume = currentVolume - 1;
-        }
+        int target = currentVolume - 1;
+        setCurrentVolume(target);
     }
 
+
     public void increaseVolume() {
-        if (currentVolume < 10) {
-            currentVolume = currentVolume + 1;
-        }
+        int target = currentVolume + 1;
+        setCurrentVolume(target);
 
 
     }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -5,91 +5,70 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class RadioTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "1",
-            "9",
-            "6",
-            "3",
-            "0"
-    })
+    @CsvSource({"1", "9", "6", "3", "0"})
     public void setprevStationTest(int volumeSound) {
         Radio curnextstat = new Radio();
         int exepceted;
-        curnextstat.currentRadioStation = volumeSound;
+        curnextstat.setCurrentRadioStation(volumeSound);
         curnextstat.prev();
         if (volumeSound > 0) {
             exepceted = volumeSound - 1;
         } else {
             exepceted = 9;
         }
-        int actual = curnextstat.currentRadioStation;
+        int actual = curnextstat.getCurrentRadioStation();
         Assertions.assertEquals(exepceted, actual);
 
     }
+
     @ParameterizedTest
-    @CsvSource({
-            "1",
-            "9",
-            "4",
-            "7",
-            "0"
-    })
+    @CsvSource({"1", "9", "4", "7", "0"})
     public void setNextStationTest(int volumeSound) {
         Radio curnextstat = new Radio();
         int exepceted;
-        curnextstat.currentRadioStation = volumeSound;
+        curnextstat.setCurrentRadioStation(volumeSound);
         curnextstat.next();
         if (volumeSound < 9) {
             exepceted = volumeSound + 1;
         } else {
             exepceted = 0;
         }
-        int actual = curnextstat.currentRadioStation;
+
+        int actual = curnextstat.getCurrentRadioStation();
         Assertions.assertEquals(exepceted, actual);
 
     }
+
     @ParameterizedTest
-    @CsvSource({
-            "1",
-            "9",
-            "4",
-            "7",
-            "10"
-    })
+    @CsvSource({"0", "9", "4", "7", "10"})
     public void setCurvolSoundTest(int volumeSound) {
         Radio curvolsound = new Radio();
         int exepceted;
-        curvolsound.currentVolume = volumeSound;
+        curvolsound.setCurrentVolume(volumeSound);
         curvolsound.increaseVolume();
         if (volumeSound < 10) {
             exepceted = volumeSound + 1;
         } else {
             exepceted = volumeSound;
         }
-        int actual = curvolsound.currentVolume;
+        int actual = curvolsound.getCurrentVolume();
         Assertions.assertEquals(exepceted, actual);
 
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "0",
-            "8",
-            "3",
-            "5",
-            "10"
-    })
+    @CsvSource({"0", "8", "3", "5", "10"})
     public void setCurvolSound2Test(int volumeSound) {
         Radio curvolsound = new Radio();
         int exepceted;
-        curvolsound.currentVolume = volumeSound;
+        curvolsound.setCurrentVolume(volumeSound);
         curvolsound.lowerVolume();
         if (volumeSound > 0) {
             exepceted = volumeSound - 1;
         } else {
             exepceted = volumeSound;
         }
-        int actual = curvolsound.currentVolume;
+        int actual = curvolsound.getCurrentVolume();
         Assertions.assertEquals(exepceted, actual);
 
     }
